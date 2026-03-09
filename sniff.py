d4b561a8870d27a09d70794a6b8bd7656edbccbb
Hydro-Cannon <joon5835@gmail.com>
2026-03-09 16:42:38 +0900

    1st sniffer

--- sniff.py
+++ sniff.py
@@ -1,3 +1,15 @@
-print('hello World')
-print('hello World2')
-print('hello World3')
+import socket
+
+s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+
+try:
+    print('Starting...')
+    while True:
+        raw_data, addr = s.recvfrom(65536)
+        print(f'addr: {addr}')
+        print(f'data: {raw_data}')
+except KeyboardInterrupt:
+    print('\nShutting Down...')
+
+finally:
+    s.close()
